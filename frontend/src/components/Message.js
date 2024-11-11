2779705f5ea7bd8e821ec10e0da14c913a466e29
Alexander DeJesus <alexdejesus336@Yahoo.com>
2024-11-11 02:29:20 -0500

    Frontend markdown support

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 
 const Message = ({ text, isUser }) => {
   return (
     <div className={`p-2 rounded-md ${isUser ? 'bg-blue-200 self-end' : 'bg-gray-200 self-start'}`}>
-      {text}
+      <ReactMarkdown>{text}</ReactMarkdown>
     </div>
   );
 };
